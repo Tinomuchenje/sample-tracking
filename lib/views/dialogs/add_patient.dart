278e9f1d27a0f7211b12b9af674353704da92af6
TinoJoelMuchenje <tinojoel90@gmail.com>
2021-09-26 03:08:50 +0200

    - Adedd more fields on patient

--- lib/views/dialogs/add_patient.dart
+++ lib/views/dialogs/add_patient.dart
@@ -6,15 +6,21 @@ import 'package:sample_tracking_system_flutter/providers/patient_provider.dart';
 import 'package:sample_tracking_system_flutter/views/widgets/custom_elevated_button.dart';
 import 'package:sample_tracking_system_flutter/views/widgets/custom_text_form_field.dart';
 
-class AddorUpdatePatientDialog extends StatelessWidget {
+class AddorUpdatePatientDialog extends StatefulWidget {
   Patient? patientData;
   AddorUpdatePatientDialog({Key? key, this.patientData}) : super(key: key);
 
-  final _formKey = GlobalKey<FormState>();
+  @override
+  State<AddorUpdatePatientDialog> createState() =>
+      _AddorUpdatePatientDialogState();
+}
 
+class _AddorUpdatePatientDialogState extends State<AddorUpdatePatientDialog> {
+  final _formKey = GlobalKey<FormState>();
+  int _value = 1;
   @override
   Widget build(BuildContext context) {
-    final Patient _patient = patientData ?? Patient();
+    final Patient _patient = widget.patientData ?? Patient();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
@@ -40,13 +46,67 @@ class AddorUpdatePatientDialog extends StatelessWidget {
                           if (value != null) _patient.lastname = value;
                         },
                       ),
+                      DropdownButton(
+                        value: _value,
+                        items: [
+                          const DropdownMenuItem(
+                            child: Text("First Item"),
+                            value: 1,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("Second Item"),
+                            value: 2,
+                          )
+                        ],
+                        // onChanged: (int value) {
+                        //   setState(() {
+                        //     // _value = value;
+                        //   });
+                        // },
+                      ),
+                      CustomTextFormField(
+                        labelText: "Date of birth",
+                        onSaved: (value) {
+                          if (value != null) _patient.dob = value;
+                        },
+                      ),
+                      CustomTextFormField(
+                        labelText: "Client",
+                        onSaved: (value) {
+                          if (value != null) _patient.client = value;
+                        },
+                      ),
+                      CustomTextFormField(
+                        labelText: "Cohort Number",
+                        onSaved: (value) {
+                          if (value != null) _patient.cohortNumber = value;
+                        },
+                      ),
+                      CustomTextFormField(
+                        labelText: "Date created",
+                        enabled: false,
+                        onSaved: (value) {
+                          if (value != null) {
+                            _patient.dateCreated = DateTime.now().toString();
+                          }
+                        },
+                      ),
+                      CustomTextFormField(
+                        enabled: false,
+                        labelText: "Date Modified",
+                        onSaved: (value) {
+                          if (value != null) {
+                            _patient.dateModified = DateTime.now().toString();
+                          }
+                        },
+                      ),
                       CustomElevatedButton(
                           labelText: "Save Patient",
                           onPressed: () {
                             if (_formKey.currentState!.validate()) {
                               _formKey.currentState!.save();
 
-                              patientData == null
+                              widget.patientData == null
                                   ? addNewSample(context, _patient)
                                   : updateSample(context, _patient);
 
