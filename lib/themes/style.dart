6166d7648851fbc04557507afa84e52579c1a723
TinoJoelMuchenje <tinojoel90@gmail.com>
2021-10-23 15:33:33 +0200

    - changed primary colour

--- lib/themes/style.dart
+++ lib/themes/style.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData appTheme() {
   const purple = Color(0xFFF72670);
   return ThemeData(
+      primarySwatch: Colors.green,
       primaryColor: purple,
       shadowColor: Colors.grey,
       appBarTheme: const AppBarTheme(backgroundColor: Colors.grey));
